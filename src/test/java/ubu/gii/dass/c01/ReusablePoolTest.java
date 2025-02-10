3a4f140dd85c00e06d7b332975fad015e4a5e5e6
aos0020 <93768166+aos0020@users.noreply.github.com>
2025-02-10 16:30:13 +0100

    Update testGetInstance

--- src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -33,6 +33,11 @@ public class ReusablePoolTest {
 	
 	public void testGetInstance() {
 
+		ReusablePool pool1 = ReusablePool.getInstance();
+		ReusablePool pool2 = ReusablePool.getInstance();
+		assertTrue(pool1 == pool2, "getInstance should return same instance");
+
+
 
 	}
 
